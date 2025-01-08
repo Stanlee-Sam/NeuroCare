bc9cb7ab37ea39217efa2bb9acd7dc4003aeec39
stanleyamunze03@gmail.com <stanleyamunze03@gmail.com>
2025-01-08 13:01:17 +0300

    Login and signup responsive complete

--- neurocare/src/pages/Login/Login.jsx
+++ neurocare/src/pages/Login/Login.jsx
@@ -2,11 +2,13 @@ import { FaGoogle } from "react-icons/fa";
 import { FaFacebookF } from "react-icons/fa6";
 import { IoMail } from "react-icons/io5";
 import { IoIosLock } from "react-icons/io";
+
 const Login = () => {
   return (
-    <section className=" md:grid place-items-center md:bg-[#D9D9D9] md:h-[100vh] h-full">
-      <div className=" place-self-center md:flex bg-white md:w-[50%] w-[100%] ">
-        <div className="md:w-3/5">
+    <section className="grid place-items-center bg-[#D9D9D9] h-screen">
+      <div className="md:flex bg-white md:w-[50%] w-full grid grid-rows-[auto_1fr] h-full md:h-[95vh] place-self-center">
+        {/* Login Form Section */}
+        <div className="md:w-3/5 md:pb-10 md:p-5">
           <div>
             <img
               className="md:w-20 w-2 invisible md:visible"
@@ -14,7 +16,7 @@ const Login = () => {
               alt=""
             />
           </div>
-          <div className="grid justify-center ">
+          <div className="grid place-items-center">
             <h1 className="text-center text-3xl text-[#608BC1] font-bold">
               Login to Account
             </h1>
@@ -24,13 +26,13 @@ const Login = () => {
             <div className="flex justify-center gap-x-4 my-2">
               <a
                 href="#"
-                className="border-2 border-gray-200 mx-1 p-2 rounded-[50%] "
+                className="border-2 border-gray-200 mx-1 p-2 rounded-[50%]"
               >
                 <FaGoogle className="w-6 h-6 text-black" />
               </a>
               <a
                 href="#"
-                className="border-2 border-gray-200 mx-1 p-2 rounded-[50%] "
+                className="border-2 border-gray-200 mx-1 p-2 rounded-[50%]"
               >
                 <FaFacebookF className="w-6 h-6 text-black" />
               </a>
@@ -43,24 +45,24 @@ const Login = () => {
             </div>
 
             <form className="grid justify-center gap-y-2">
-              <div className="flex items-center border-2 bg-gray-100 border-gray-200 p-2 rounded-lg ">
+              <div className="flex items-center border-2 bg-gray-100 border-gray-200 p-2 rounded-lg">
                 <IoMail className="text-2xl text-gray-400 font-light" />
                 <input
-                  className="font-medium outline-none rounded-lg p-2 text-sm bg-gray-100 "
+                  className="font-medium outline-none rounded-lg p-2 text-sm bg-gray-100"
                   type="text"
                   placeholder="Email"
                 />
               </div>
-              <div className="flex items-center border-2 bg-gray-100 border-gray-200 p-2 rounded-lg ">
+              <div className="flex items-center border-2 bg-gray-100 border-gray-200 p-2 rounded-lg">
                 <IoIosLock className="text-2xl text-gray-400 font-light" />
                 <input
-                  className="font-medium outline-none rounded-lg p-2 text-sm bg-gray-100 "
+                  className="font-medium outline-none rounded-lg p-2 text-sm bg-gray-100"
                   type="password"
                   placeholder="Password"
                 />
               </div>
             </form>
-            <div className="flex justify-evenly w-64 my-5 ">
+            <div className="flex justify-evenly w-64 md:my-5 my-3">
               <label
                 htmlFor="remember"
                 className="text-xs flex items-center gap-1"
@@ -74,7 +76,7 @@ const Login = () => {
             <div className="place-self-center">
               <button
                 type="submit"
-                className=" border-2 border-[#608BC1] inline-block text-black hover:bg-[#608BC1] rounded-full px-12 py-2 font-semibold mb-2 hover:text-white w-full max-[750px]:mb-10"
+                className="border-2 border-[#608BC1] inline-block text-black hover:bg-[#608BC1] rounded-full px-12 py-2 font-semibold md:mb-2 hover:text-white w-full max-[750px]:mb-10"
               >
                 Login
               </button>
@@ -82,17 +84,19 @@ const Login = () => {
             </div>
           </div>
         </div>
-        <div className="bg-[#608BC1] md:rounded-l-xl max-[770px]:rounded-t-full  md:w-2/5 grid place-items-center ">
+
+        {/* Blue Section */}
+        <div className="bg-[#608BC1] md:rounded-l-xl max-[770px]:rounded-t-full md:w-2/5 grid place-items-center row-start md:row-start-auto">
           <div className="place-items-center">
-            <h1 className="text-3xl text-white mb-2 font-bold">
+            <h1 className="md:text-3xl text-white md:mb-2 font-bold text-[18px]">
               Hello, Friend!
             </h1>
             <div className="border-2 place-self-center my-5 border-white inline-block mb-2 w-10"></div>
 
-            <p className="text-center px-2 mb-3 text-white">
+            <p className="text-center px-2 md:mb-3 text-white text-[12px] md:text-[20px]">
               Enter your personal details and start your journey with us
             </p>
-            <button className=" border-2 border-white inline-block text-white hover:bg-white rounded-full px-12 py-2 font-semibold mt-5 hover:text-[#608BC1] w-full">
+            <button className="border-2 border-white inline-block text-white hover:bg-white rounded-full px-12 py-2 font-semibold mt-5 hover:text-[#608BC1] w-full">
               Sign Up
             </button>
           </div>
--- neurocare/src/pages/Sign up/SignIn.jsx
+++ neurocare/src/pages/Sign up/SignIn.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { FaGoogle } from "react-icons/fa";
 import { FaFacebookF } from "react-icons/fa6";
 import { IoMail } from "react-icons/io5";
