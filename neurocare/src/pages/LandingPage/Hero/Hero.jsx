f3350793fcd786d61815a05706e950adffbe8860
Stanley Amunze <stanleyamunze03@gmail.com>
2025-10-10 00:54:13 +0300

    hero section modification

--- neurocare/src/pages/LandingPage/Hero/Hero.jsx
+++ neurocare/src/pages/LandingPage/Hero/Hero.jsx
@@ -1,6 +1,7 @@
 import "./Hero.css";
 import { Link } from "react-router-dom";
 import logoHero  from "../../../../src/assets/Logo-hero.png";
+import MainLogo from "../../../../src/assets/logo-hero-main.png"
 
 const Hero = () => {
   return (
@@ -11,30 +12,34 @@ const Hero = () => {
           <div className="logo">
             <img src={logoHero} alt="" />
           </div>
-          <div className="buttons">
-            <button className="bg-[#77DD77] text-black-600 p-3 rounded-lg text-[10px] md:text-[15px] font-bold hover:bg-gray-100 transition">
+          {/* <div className="buttons">
+            <button className="bg-[#77DD77] text-black-600 p-3 rounded-lg text-[10px] md:text-[15px] font-bold hover:bg-gray-100 transition cursor-pointer">
               <Link to="/signup">Sign up</Link>
             </button>
             <button className="log-in-btn bg-[#608BC1] text-black-600 p-3 text-[10px] md:text-[15px] rounded-lg font-bold hover:bg-gray-100 transition">
               Log in
             </button>
-          </div>
+          </div> */}
         </div>
-        <div>
-          <div className="banner ">
+        <div className="flex flex-col items-center">
+          <div 
+          className="flex flex-col items-center relative"
+          >
             <img
-              className=" "
-              src="../../../../src/assets/logo-hero-main.png"
+              className="w-[300px] md:w-[380px] "
+              src={MainLogo}
               alt=""
             />
-            <h1 className="absolute left-[22%] md:left-[36%]">
+            <h1 className=" text-white font-bold text-[15px] absolute top-[60%]">
               Track and Support Your Well-being
             </h1>
-            <button className="bg-[#77DD77] text-black-600 p-3 rounded-lg text-[10px] md:text-[15px] font-bold hover:bg-gray-100 transition absolute bottom-[26%] left-[46%] text-md ">
+            <button 
+            className="bg-[#77DD77] text-black-600 p-3 rounded-lg text-[10px] md:text-[15px] font-bold hover:bg-gray-100 transition  text-md absolute top-[75%] "
+            >
               <Link to="/login">Get Started</Link>
             </button>
           </div>
-          <div>
+          <div className="flex flex-col items-center">
             <span className="mouse">
               <span className="mouse-wheel"></span>
             </span>
